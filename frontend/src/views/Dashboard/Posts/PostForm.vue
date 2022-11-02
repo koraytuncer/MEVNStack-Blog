76a9e327499538ca6242b12ab572343bc8d9bb7b
koraytuncer <ktuncerr@gmail.com>
2022-11-02 21:28:48 +0300

    Post Completed

--- frontend/src/views/Dashboard/Posts/PostForm.vue
+++ frontend/src/views/Dashboard/Posts/PostForm.vue
@@ -14,7 +14,12 @@
 
         <div class="mb-3">
           <label class="form-label">Kategori</label>
-          <select v-model="post.category" @change="onChangeSection($event)" name="category" class="form-select">
+          <select
+            v-model="post.category"
+            @change="onChangeSection($event)"
+            name="category"
+            class="form-select"
+          >
             <option value="" disabled selected>Lütfen Seçiniz</option>
             <option v-for="(item, index) in categories" :key="index">
               {{ item.title }}
@@ -37,6 +42,7 @@
             v-model:content="post.description"
             contentType="html"
             theme="snow"
+            ref="quill"
           ></QuillEditor>
 
           <div class="col-6 col-sm-4 col-md-2 col-xl py-3 float-end">
@@ -50,25 +56,49 @@
 <script>
 import { QuillEditor } from "@vueup/vue-quill";
 import "@vueup/vue-quill/dist/vue-quill.snow.css";
-import {onMounted,computed} from "vue"
+import { onMounted, computed, ref, watch, nextTick } from "vue";
 import { useStore } from "vuex";
 export default {
   name: "Post-Form",
   components: {
     QuillEditor,
   },
-  props: ["post", "onSubmit","onChangeSection"],
-  setup(){
+  props: ["post", "onSubmit", "onChangeSection"],
+  setup(props) {
     const store = useStore();
-    
+    const quill = ref(null);
+    props.post.description = "";
+
+    let newContent = "";
+
+    watch(props.post.description, (newValue) => {
+      newContent = newValue;
+      props.post.description = newValue;
+    });
+
+    watch(
+      () => props.post.description,
+      (newValue) => {
+        if (newContent === newValue) return;
+
+        quill.value.setHTML(newValue);
+
+        nextTick(() => {
+          let q = quill.value.getQuill();
+          q.setSelection(newValue.length, 0, "api");
+          q.focus();
+        });
+      }
+    );
+
     const categories = computed(() => store.getters.getCategories);
     onMounted(() => store.dispatch("initCategories"));
 
-
-    return{
-        categories,
-    }
-  }
+    return {
+      categories,
+      quill,
+    };
+  },
 };
 </script>
 <style>
